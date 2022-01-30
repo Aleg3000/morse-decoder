0a62423eca8430c63ebe5fea4ffcf93359ca951f
Oleg Ganin <olejaaaa@gmail.com>
2022-01-30 15:16:02 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -38,8 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arr = [];
+    for (let i = 0; i < expr.length / 10; i++){  
+        arr.push(expr.substr(i * 10, 10));
+    }
+    result = arr.map(el => {
+      if (el == '**********') return ' ';
+      else {
+        let letter = [];
+        for (let i = 0; i < 5; i++) {
+            letter.push(el.substr(i * 2, 2));
+        }
+        return letter;
+      }
+    })
+  for (let i = 0; i < result.length; i++){
+    if (result[i] == ' ') continue;
+    else {
+      result[i] = result[i].map(el => {
+      switch(el){
+        case '10':
+          return '.';
+        case '11':
+          return '-';  
+        default:
+          return ' '
+    }
+  }).join('').trim()
+    }
+  }
+  result = result.map(el => {
+    if (el == ' ') return ' ';
+    else return MORSE_TABLE[el] 
+  }).join('')
+    return result
+  }
 
 module.exports = {
     decode
